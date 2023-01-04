fe3e10731772919806c7f5a264709305cfead6c9
gnara25 <mandhikafebriansyah25@gmail.com>
2023-01-04 19:21:41 +0700

    menambahkan produk

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+//pruduk
+Route::get('/product', function () {
+    return view('produk.produk');
+});
