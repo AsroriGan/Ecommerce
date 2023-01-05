c7901746012d4f93c8162e5b082fe8f55c41f700
AsroriGan <masroridhani@gmail.com>
2023-01-05 12:35:36 +0700

    tamplatelogin

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function blog(){
+        return view('landingpage.landingBlog.blog');
+    }
+}
