ba3da73dbdd56a2c93c79c1ad65d70a8496e01a5
unknown <unluading123@gmail.com>
2023-01-05 21:39:48 +0700

    menghapus route gg

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ Route::get('/beranda', function () {
 });
 
 //pruduk
-Route::get('/product', function () {return view('produk.produk');});
 Route::get('/produk',[ProdukController::class,'produk'])->name('produk');
 Route::get('/detail',[ProdukController::class,'detail'])->name('detail');
 
@@ -35,17 +34,10 @@ Route::get('/detail',[ProdukController::class,'detail'])->name('detail');
 Route::get('/blog', [AdminBlogController::class, 'blog']);
 Route::get('/detailblog', [AdminBlogController::class, 'detailblog']);
 
-Route::get('/product', function () {
-    return view('produk.produk');
-});
-<<<<<<< HEAD
-=======
-
 Route::get('/test', function () {
     return view('landingpage.testsofyan');
 });
 
->>>>>>> 5043850844a0de84315e58ba11f3a003bc1af32b
 Route::get('/', [LandingpageController::class, 'home']);
 //Kategori
 Route::get('/kategori',[KategoriController::class,'index'])->name('index.kategori');
