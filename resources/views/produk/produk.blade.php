19f8d93e561f3ff8bb1ede1a4399c7cae05fc895
gnara25 <mandhikafebriansyah25@gmail.com>
2023-01-06 18:17:07 +0700

    tes

--- resources/views/produk/produk.blade.php
+++ resources/views/produk/produk.blade.php
@@ -9,7 +9,7 @@
  <!DOCTYPE html>
  <html lang="en">
  
- <link rel="icon" href="assets/images/favicon/favicon.png" sizes="32x32" />
+    <link rel="icon" href="assets/images/favicon/favicon.png" sizes="32x32" />
     <link rel="apple-touch-icon" href="assets/images/favicon/favicon.png" />
     <meta name="msapplication-TileImage" content="assets/images/favicon/favicon.png" />
 
@@ -2077,31 +2077,6 @@
 
      <!-- Script -->
 
-     <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
-    <script src="assets/js/vendor/popper.min.js"></script>
-    <script src="assets/js/vendor/bootstrap.min.js"></script>
-    <script src="assets/js/vendor/jquery-migrate-3.3.0.min.js"></script>
-    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
-
-    <!--Plugins JS-->
-    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
-    <script src="assets/js/plugins/countdownTimer.min.js"></script>
-    <script src="assets/js/plugins/scrollup.js"></script>
-    <script src="assets/js/plugins/jquery.zoom.min.js"></script>
-    <script src="assets/js/plugins/slick.min.js"></script>
-    <script src="assets/js/plugins/infiniteslidev2.js"></script>
-    <script src="assets/js/vendor/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/plugins/jquery.sticky-sidebar.js"></script>
-    <!-- Google translate Js -->
-    <script src="assets/js/vendor/google-translate.js"></script>
-    <script>
-        function googleTranslateElementInit() {
-            new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
-        }
-    </script>
-    <!-- Main Js -->
-    <script src="assets/js/vendor/index.js"></script>
-    <script src="assets/js/main.js"></script>
      @include('layouts.script')
 
      <!-- End Script
