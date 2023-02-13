90305605e180c11ac1896550ab44e64b9e9b287f
Jukiks45 <unluading123@gmail.com>
2023-02-13 13:54:10 +0700

    route pesanan masuk

---
+++ resources/views/dashboardadmin/pesananmasuk/index.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Head Star -->
+
+@include('layoutsadmin.head')
+
+<!-- End Head -->
+
+<body>
+
+    <div class="main-wrapper">
+
+        <!-- Navbar Star -->
+
+        @include('layoutsadmin.navbar')
+
+        <!-- End Navbar -->
+
+        <!-- Star Sidebar -->
+
+        @include('layoutsadmin.sidebar')
+
+        <!-- End sidebar -->
+
+
+        <div class="page-wrapper">
+            <div class="content container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="page-header">
+                            <div class="row">
+                                <div class="col">
+                                    <h3 class="page-title">Pesanan Masuk</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="table-responsive" id="tampilanmerk">
+                                    <table class="datatable table table-stripped" id="myTable">
+                                        <thead>
+                                            <tr>
+                                                <th>No.</th>
+                                                <th>Tanggal Pesanan</th>
+                                                <th>Foto</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {{-- @foreach ($data as $data) --}}
+                                                <tr>
+                                                    <td scope="row"></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td>
+                                                        <button class="btn btn-sm  btn-white text-success me-2"
+                                                            data-bs-toggle="modal" data-bs-target="#modalmerek"
+                                                            onclick="editview()"><i
+                                                                class="far fa-edit me-1"></i>
+                                                            Edit</button>
+                                                    </td>
+                                                </tr>
+                                            {{-- @endforeach --}}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="modalmerek" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true" style="display: none;">
+        <div class="modal-dialog">
+            <div class="modal-content" id="form_modal">
+                <div class="modal-header">
+                    <h4 class="modal-title">Tambah Merek</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body p-4">
+                    <div class="row">
+                        <form id="postmerk" action="{{ url('storemerek') }}" method="POST"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <div class="col-md-12">
+                                <div class="mb-3">
+                                    <label for="field-3" class="form-label">Merek
+                                        :</label>
+                                    <input type="text" id="nama_merek" name="nama_merek" class="form-control"
+                                        id="field-3" placeholder="Masukan Nama Merek">
+                                    <div class="invalid-feedback" id="msg_merek">
+
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="field-3" class="form-label">Foto
+                                        :</label>
+                                    <input type="file" id="foto_merek" name="foto_merek" class="form-control"
+                                        id="field-3">
+                                    <div class="invalid-feedback" id="msg_foto"></div>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary waves-effect"
+                            data-bs-dismiss="modal">Kembali</button>
+                        <button type="button" class="btn btn-info waves-effect waves-light"
+                            onclick="insertdata()">Tambah
+                        </button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Star Script -->
+
+    @include('layoutsadmin.script')
+
+    <script>
+        //
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
+    <!-- End Script -->
+
+</body>
+
+</html>
